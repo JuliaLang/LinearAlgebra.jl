3fb630d26655188db9da0fe4ba58c3f7c3ba3a86
Dilum Aluthge <dilum@aluthge.com>
2022-09-21 20:49:58 -0400

    At the beginning of the test suite, delete all of the old LinearAlgebra methods before we do `import LinearAlgebra`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,35 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+function delete_methods_from(
+        from_module_name::AbstractString,
+        modules_to_search = values(Base.loaded_modules),
+    )
+    num_deleted_methods = 0
+    for m in modules_to_search
+        m_names = names(m; all = true, imported = true)
+        for name in m_names
+            if !hasproperty(m, name)
+                continue
+            end
+            generic_function = getproperty(m, name)
+            for method in methods(generic_function)
+                if string(method.module) == from_module_name
+                    @debug "$(num_deleted_methods). Deleting method: $(method)"
+                    Base.delete_method(method)
+                    num_deleted_methods += 1
+                end
+            end
+        end
+    end
+    return num_deleted_methods
+end
+
+# Make sure to run this before we do `import LinearAlgebra`
+delete_methods_from("LinearAlgebra")
+
+# Load our version of LinearAlgebra
+import LinearAlgebra
+
 for file in readlines(joinpath(@__DIR__, "testgroups"))
     include(file * ".jl")
 end
