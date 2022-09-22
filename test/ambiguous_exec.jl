1856caed4fb4496c548c441425faccad6c797dde
Dilum Aluthge <dilum@aluthge.com>
2022-09-22 14:17:44 -0400

    Update ambiguous_exec.jl

--- test/ambiguous_exec.jl
+++ test/ambiguous_exec.jl
@@ -1,5 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+include("util.jl")
+
 # Make sure to run this before we do `import LinearAlgebra`
 delete_methods_from("LinearAlgebra")
 
