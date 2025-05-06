34b8d7abe01cd02e2206d45823e999661e4b8ccf
Jishnu Bhattacharya <jishnub.github@gmail.com>
2025-05-06 14:56:12 +0530

    Unreference `LinearAlgebra` only if env var is true

--- test/prune_old_LA.jl
+++ test/prune_old_LA.jl
@@ -1,55 +1,54 @@
-methods_to_delete =
-[
-:adjoint
-:transpose
-:inv
-:literal_pow
-:\
-:/
-:isapprox
-:copyto!
-:*
-:muladd
-:copyto!
-:isone
-:kron!
-:kron
-:^
-:exp
-:cis
-:log
-:sqrt
-:cbrt
-:inv
-:cos
-:sin
-:sincos
-:tan
-:cosh
-:sinh
-:tanh
-:acos
-:asin
-:atan
-:acosh
-:asinh
-:atanh
-:sec
-:sech
-:csc
-:csch
-:cot
-:coth
-:asec
-:asech
-:acsc
-:acot
-:acoth
-:acsch
-]
-
-
 let
+    methods_to_delete =
+    [
+    :adjoint
+    :transpose
+    :inv
+    :literal_pow
+    :\
+    :/
+    :isapprox
+    :copyto!
+    :*
+    :muladd
+    :copyto!
+    :isone
+    :kron!
+    :kron
+    :^
+    :exp
+    :cis
+    :log
+    :sqrt
+    :cbrt
+    :inv
+    :cos
+    :sin
+    :sincos
+    :tan
+    :cosh
+    :sinh
+    :tanh
+    :acos
+    :asin
+    :atan
+    :acosh
+    :asinh
+    :atanh
+    :sec
+    :sech
+    :csc
+    :csch
+    :cot
+    :coth
+    :asec
+    :asech
+    :acsc
+    :acot
+    :acoth
+    :acsch
+    ]
+
     prune_old_LA = parse(Bool, get(ENV, "JULIA_PRUNE_OLD_LA", "false"))
     LinalgSysImg = Base.PkgId(Base.UUID("37e2e46d-f89d-539d-b4ee-838fcccc9c8e"), "LinearAlgebra")
     LA = get(Base.loaded_modules, LinalgSysImg, nothing)
@@ -74,7 +73,7 @@ let
     if LA !== nothing && prune_old_LA
         @assert !hasmethod(*, Tuple{Matrix{Float64}, Matrix{Float64}})
     end
-    Base.unreference_module(LinalgSysImg)
+    prune_old_LA && Base.unreference_module(LinalgSysImg)
 end
 
 pruned_old_LA = true
