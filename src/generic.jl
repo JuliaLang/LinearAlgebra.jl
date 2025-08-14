29155d4506497b764e6ced977b99b6373266076f
Lorenzo Van Munoz <66997677+lxvm@users.noreply.github.com>
2025-08-14 13:08:18 -0400

    Update generic.jl
    use `convert` as a more generic constructor

--- src/generic.jl
+++ src/generic.jl
@@ -1846,7 +1846,7 @@ julia> det(BigInt[1 0; 2 2]) # exact integer determinant
 function det(A::AbstractMatrix{T}) where {T}
     if istriu(A) || istril(A)
         S = promote_type(T, typeof((one(T)*zero(T) + zero(T))/one(T)))
-        return prod(S, @view A[diagind(A)])
+        return prod(Base.Fix1(convert, S), @view A[diagind(A)])
     end
     return det(lu(A; check = false))
 end
