7900265538d132acf0878494573d55cc7c64f8ec
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2026-03-16 10:38:36 +0100

    Generalize zero in Givens algorithm

--- src/givens.jl
+++ src/givens.jl
@@ -77,7 +77,7 @@ floatmin2(::Type{T}) where {T} = (twopar = 2one(T); twopar^trunc(Integer,log(flo
 function givensAlgorithm(f::T, g::T) where T<:AbstractFloat
     onepar = one(T)
     T0 = typeof(onepar) # dimensionless
-    zeropar = T0(zero(T)) # must be dimensionless
+    zeropar = zero(onepar) # must be dimensionless
 
     # need both dimensionful and dimensionless versions of these:
     safmn2 = floatmin2(T0)
