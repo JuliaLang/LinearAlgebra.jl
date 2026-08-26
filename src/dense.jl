4b9f5511d877865260a2b5e35725d2833d921461
Trevor Loe <74470774+trevorgloe@users.noreply.github.com>
2026-08-26 15:44:49 -0700

    Update src/dense.jl
    Co-authored-by: Steven G. Johnson <stevenj@mit.edu>

--- src/dense.jl
+++ src/dense.jl
@@ -1913,9 +1913,7 @@ function nullspace(A::AbstractVecOrMat; atol::Real=0, rtol::Real = (min(size(A, 
     m, n = size(A, 1), size(A, 2)
     (m == 0 || n == 0) && return Matrix{eigtype(eltype(A))}(I, n, n)
     SVD = svd(A; full=true)
-    tol = max(atol, SVD.S[1]*rtol)
-    indstart = sum(s -> s .> tol, SVD.S) + 1
-    return copy((@view SVD.Vt[indstart:end,:])')
+return nullspace(SVD; rtol, atol)
 end
 
 """
